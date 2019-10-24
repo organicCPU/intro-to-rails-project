8ac18428195bfd110ef14a0f63bea427ef181e04
mfalcetta <44413306+mfalcetta@users.noreply.github.com>
2019-10-24 16:02:27 -0500

    Fix typo in routes and fix ordering

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :pages, only: :show
-  get '/permalink', to: 'pages#permalink', as: 'permalink'
   # get '/static', to: root_path
 
   resources :artists, only: %i[index show]
   resources :cards, only: %i[index show]
   resources :colors, only: %i[index show]
   resources :rulings, only: %i[index show]
+
+  # Catch-all
+  resources :pages, only: :show
+  get '/:permalink', to: 'pages#permalink', as: 'permalink'
 
   root to: 'cards#index'
 end
